fd46a70a9bb3d04ec17d53c91ad416aa36a70010
krishbhutani <123577916+krishbhutani@users.noreply.github.com>
2024-09-04 18:13:58 +0000

    Conversions and Operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+// Conversion to Number
+let score = "ssdfs"
+let scoreNumber = Number(score)
+// console.log(scoreNumber)
+// console.log(typeof(scoreNumber))
+
+/*
+"33" => 33
+"33ans" => NaN
+""  => 0
+null => 0
+undefined => NaN
+"dhg" => NaN
+*/
+
+// Conversion to Boolean
+let bool = 1
+let ans = Boolean(bool)
+console.log(ans)
+console.log(typeof(ans))
+
+/*
+1 => true ; 0 => false
+"" => false
+"sjdfh" => true
+*/
---
+++ 01_basics/04_operations.js
@@ -0,0 +1,9 @@
+// Concatenation of Strings
+let str1 = "Krish"
+let str2 = " Bhutani"
+//console.log(str1+str2)
+
+// Merging Number and a String
+console.log("2" + 4 + 6)
+console.log(2 + 4 + "6")
+// NOTE := We can say that priority of string >> Number
